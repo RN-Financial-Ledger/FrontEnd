f4a1b327dcf289af0ba2abe44a0b9efd8f571597
gustmd213 <51289147+gustmd213@users.noreply.github.com>
2022-10-02 18:40:24 +0900

    test

---
+++ MyComponents.tsx
@@ -0,0 +1,31 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {type PropsWithChildren} from 'react';
+import {Text, View} from 'react-native';
+import tw from 'tailwind-react-native-classnames';
+
+const Section: React.FC<
+  PropsWithChildren<{
+    title: string;
+  }>
+> = ({title}) => {
+  return (
+    <View style={tw`p-4 android:pt-2 bg-red-300 flex-row`}>
+      <Text style={tw`text-md tracking-wide`}>{title}</Text>
+    </View>
+  );
+};
+
+const MyComponents = () => {
+  return <Section title="Hello Wosdsdrld" />;
+};
+
+export default MyComponents;
